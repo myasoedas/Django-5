dac428ed62d2a77d8e99a52b1ab6690554c1492b
myasoedas <myasoedas@yandex.ru>
2025-06-08 15:58:54 +0300

    blog/models: добавил поля created, updated для автоматического учета времени создания и измнения поста.

--- 01-Blog/blog-project/mysite/blog/models.py
+++ 01-Blog/blog-project/mysite/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.functions import Now
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -7,9 +7,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     body = models.TextField()
 
-    publish = models.DateTimeField(
-        db_default=Now()
-    )
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
