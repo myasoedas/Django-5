6c53ba66ce4fab9d6c7b734994dbb2da600b42a4
myasoedas <myasoedas@yandex.ru>
2025-06-10 11:02:17 +0300

    blog/models: добавил связь многие к одному в модели Post в поле author.

--- 01-Blog/blog-project/mysite/blog/models.py
+++ 01-Blog/blog-project/mysite/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -9,12 +10,17 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2,                # Храним 2 символа (DF или PB)
+        max_length=2,               # Храним 2 символа (DF или PB)
         choices=Status.choices,     # Список допустимых значений из перечисления
         default=Status.DRAFT,       # Значение по умолчанию — черновик
     )
